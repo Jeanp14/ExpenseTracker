f7301778687f8b202188cd04143efe2f605819e2
Jean-Pamphile Goutondji <jpgoutondji@yahoo.ca>
2023-05-12 19:30:31 -0400

    added some styling and icons for the tabs

--- App.tsx
+++ App.tsx
@@ -3,19 +3,47 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/Styles';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 const ExpensesOverview = () => {
   return(
-    <BottomTabs.Navigator screenOptions={{}}>
-      <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses}/>
-      <BottomTabs.Screen name='AllExpenses' component={AllExpenses}/>
+    <BottomTabs.Navigator 
+      screenOptions={{
+        headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        headerTintColor: 'white',
+        tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        tabBarActiveTintColor: GlobalStyles.colors.accent500
+      }}>
+      <BottomTabs.Screen 
+        name='RecentExpenses' 
+        component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='hourglass' color={color} size={size}/>
+          )
+        }}
+      />
+      <BottomTabs.Screen 
+        name='AllExpenses' 
+        component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='calendar' color={color} size={size}/>
+          ) 
+        }}
+      />
     </BottomTabs.Navigator>
   )
 }
